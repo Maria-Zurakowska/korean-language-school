20e88c6e55c311ffc0c42dce6b3f5602793794e4
Maria Żurakowska <mary_masza@MacBook-Air-Maria.local>
2024-05-17 15:32:01 +0900

    First commit

---
+++ src/main/java/com/koreanlanguageschool/KoreanLanguageSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.koreanlanguageschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanLanguageSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreanLanguageSchoolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/koreanlanguageschool/Test.java
@@ -0,0 +1,5 @@
+package com.koreanlanguageschool;
+
+public class Test {
+    // test class
+}
